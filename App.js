2931caec869d09bcc9dcd3688432d38e7c9ec064
ken972 <85898797+ken972@users.noreply.github.com>
2021-06-28 07:48:59 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import PostForm from './Components/PostForm'
+//import Greet from './Components/Greet'
+import Stylesheet from './Components/Stylesheet'
+//import GetList from './Components/GetList'
+//import Form from './Components/Form'
+//import Gen from './Components/Gen'
+//import Class from './Components/Class'
+//import List from './Components/List'
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm />
+      <Stylesheet />
+      {/*<GetList />*/}
+      {/*<Form />*/}
+      {/*<Gen />*/}
+      {/*<List />*/}
+      {/*<Class />*/}
+      {/*<Greet />*/}
+    </div>
+  );   
+}
+
+export default App;
